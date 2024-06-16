f99682bad7e819ed8cee4d2c282f387774cb1f25
Mariana345 <dekhtiarenko.mariana@gmail.com>
2024-06-16 20:11:51 +0300

    Initial Commit

---
+++ CarVision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarVision
+//
+//  Created by Mariana Dekhtiarenko on 16.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
