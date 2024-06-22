c32ed2ad70bfac883e225f45ea7c2c14bf3076c5
Mariana345 <dekhtiarenko.mariana@gmail.com>
2024-06-22 23:39:53 +0300

    Created CurrentVehicleController

---
+++ CarVision/CurrentVehicleController.swift
@@ -0,0 +1,29 @@
+//
+//  CurrentVehicleController.swift
+//  CarVision
+//
+//  Created by Mariana Dekhtiarenko on 22.06.2024.
+//
+
+import UIKit
+
+class CurrentVehicleController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
