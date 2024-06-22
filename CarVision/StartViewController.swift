456c92a7dc3da271a084772dd0d5bc5d921334aa
Mariana345 <dekhtiarenko.mariana@gmail.com>
2024-06-22 13:51:32 +0300

    new changes

--- CarVision/StartViewController.swift
+++ CarVision/StartViewController.swift
@@ -6,24 +6,93 @@
 //
 
 import UIKit
+import Vision
 
-class StartViewController: UIViewController {
+class StartViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var recognizeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Start Controller"
 
-        // Do any additional setup after loading the view.
+        recognizeButton.layer.cornerRadius = 10
+        recognizeButton.layer.masksToBounds = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func recognizeButtonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Choose an option", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { _ in
+            self.openCamera()
+        }))
+        alert.addAction(UIAlertAction(title: "Choose from Library", style: .default, handler: { _ in
+            self.openPhotoLibrary()
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
     }
-    */
+    
+    func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
 
+    func openPhotoLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        if let image = info[.originalImage] as? UIImage {
+            recognizeCar(in: image)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    func recognizeCar(in image: UIImage) {
+        guard let model = try? VNCoreMLModel(for: CarsImgClassifier().model) else {
+            fatalError("Could not load model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            if let results = request.results as? [VNClassificationObservation] {
+                if let topResult = results.first {
+                    let carModel = topResult.identifier
+                    print("Recognized car model: \(carModel)")
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(title: "Recognition Result", message: "Car: \(carModel)", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            }
+        }
+        
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("Could not convert UIImage to CIImage")
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+        DispatchQueue.global().async {
+            do {
+                try handler.perform([request])
+            } catch {
+                print("Failed to perform request: \(error)")
+            }
+        }
+    }
 }
+
--- CarVision/ViewController.swift
+++ CarVision/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var squareView: UIView!
+    
+    @IBOutlet private weak var squareWhiteView: UIView!
+    
+    @IBOutlet private weak var getStartedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        squareView.layer.cornerRadius = 30
+        squareView.layer.masksToBounds = true
+        let path = UIBezierPath(roundedRect: squareWhiteView.bounds,
+                                byRoundingCorners: [.topLeft, .topRight],
+                                cornerRadii: CGSize(width: 30, height: 30))
+        
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        squareWhiteView.layer.mask = mask
+        getStartedButton.layer.cornerRadius = 15
+        getStartedButton.layer.masksToBounds = true
+        
     }
-
-
+    
 }
-
