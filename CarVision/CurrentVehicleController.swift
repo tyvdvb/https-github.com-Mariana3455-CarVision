11fb2b855dfc89822c08a794041499d4572e6a13
tyvdvb <ira1980nazar@gmail.com>
2024-06-23 12:49:30 -0400

    add arkit + 3d models

--- CarVision/CurrentVehicleController.swift
+++ CarVision/CurrentVehicleController.swift
@@ -1,18 +1,19 @@
 import UIKit
 
 class CurrentVehicleController: UIViewController {
-
+    
     @IBOutlet private weak var carImage: UIImageView!
     @IBOutlet private weak var carName: UILabel!
     @IBOutlet private weak var carModel: UILabel!
     @IBOutlet private weak var carTransmission: UILabel!
     @IBOutlet private weak var carNumOfDoors: UILabel!
     @IBOutlet private weak var saveCarInfoButton: UIButton!
-
+    
+    @IBOutlet weak var viewARButton: UIButton!
     var recognizedCarModel: String?
     var recognizedCarImage: UIImage?
     var carDataParser: CarDataParser?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         carModel.text = recognizedCarModel
@@ -45,33 +46,51 @@ class CurrentVehicleController: UIViewController {
             }
         }
     }
-
+    
     @IBAction func saveCarInfoButtonTapped(_ sender: UIButton) {
         guard let carModel = recognizedCarModel, let carImage = recognizedCarImage else { return }
         UserDefaults.standard.set(carModel, forKey: "savedCarModel")
-
+        
         if let imageData = carImage.pngData() {
             UserDefaults.standard.set(imageData, forKey: "savedCarImage")
         }
-
+        
         if let carDetails = carDataParser?.carDetails {
             UserDefaults.standard.set(carDetails, forKey: "savedCarDetails")
         }
-
+        
         showAlert(title: "Success", message: "Car information saved successfully.")
         saveCarInfoButton.setImage(UIImage(systemName: "bookmark.fill"), for: .normal)
     }
-
+    
     func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
-
+    
     private func printCarDetails(_ details: [String: String]) {
         print("Car Details:")
         for (key, value) in details {
             print("\(key): \(value)")
         }
     }
+    
+    
+    
+    @IBAction func viewARButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "showARView", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showARView" {
+            if let destinationVC = segue.destination as? ARViewController {
+                destinationVC.recognizedCarModel = recognizedCarModel
+                destinationVC.recognizedCarImage = recognizedCarImage
+            }
+        }
+    }
+    
+    
+    
 }
