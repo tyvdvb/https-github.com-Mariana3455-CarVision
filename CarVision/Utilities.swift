50b77cb1e8948f2d3ceb0079cdc9ba7f0c290766
tyvdvb <ira1980nazar@gmail.com>
2024-06-24 04:03:52 -0400

    add ar

---
+++ CarVision/Utilities.swift
@@ -0,0 +1,15 @@
+//
+//  Utilities.swift
+//  CarVision
+//
+//  Created by Ira Nazar on 2024-06-24.
+//
+
+
+import Foundation
+import SceneKit
+
+
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi/180}
+}
